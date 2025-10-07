dfc2f38a120dc55574d29eeda3039ada1c4daf81
Keerthika <nkeerthika09@gmail.com>
2025-10-07 15:55:40 +0530

    Range XOR Query

---
+++ Range Queries/RangeXOR_SegmentTree.cpp
@@ -0,0 +1,43 @@
+// SEGMENT TREE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+long findXOR(vector<long> &tree, int idx, int left, int right, int ql, int qr){
+    if(ql>right || qr<left){
+        return 0;
+    }
+    if(left>=ql && right<=qr){
+        return tree[idx];
+    }
+    int mid = (left+right)/2;
+    return findXOR(tree, idx*2, left, mid, ql, qr)^findXOR(tree, idx*2+1, mid+1, right, ql, qr);
+}
+
+
+int main() {
+    int n,q;
+    cin>>n>>q;
+    int lg = 0;
+    while((1<<lg)<n){
+        lg++;
+    }
+    int len = 1<<lg;
+    vector<long> tree(len*2,0);
+    for(int i=len;i<len+n;i++){
+        cin>>tree[i];
+    }
+    for(int i=len-1;i>0;i--){
+        tree[i]=tree[2*i]^tree[2*i+1];
+    }
+    int ql,qr;
+    long ans;
+    while(q-->0){
+        cin>>ql>>qr;
+        ql--;
+        qr--;
+        ans = findXOR(tree,1,len,len*2-1,ql+len,qr+len);
+        cout<<ans<<endl;
+    }
+    return 0;
+}

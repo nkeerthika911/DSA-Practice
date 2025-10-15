52bc132fd2677f62eb96891e9e3eb40068914d5f
Keerthika <nkeerthika09@gmail.com>
2025-10-15 17:21:50 +0530

    Bitwise Reversion

---
+++ Bit Manipulation/BitwiseReverse.java
@@ -0,0 +1,28 @@
+// Refer
+// https://youtu.be/9lg3FUXDn4E 
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T-->0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int flag=0;
+            while(a!=0 || b!=0 || c!=0){
+                if((a%2 + b%2 + c%2)==2){
+                    System.out.println("NO");
+                    flag=1;
+                    break;
+                }
+                a=a>>1;
+                b=b>>1;
+                c=c>>1;
+            }
+            if(flag==0) System.out.println("YES");
+        }
+    }
+}
